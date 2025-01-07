04ee20237ce1f84a0cb5f059d5e2e2306ff4ad4e
maheshrc27 <maheshrc27@gmail.com>
2025-01-07 23:09:40 +0545

    improve posting history error message

--- internal/transfer/instagram_transfer.go
+++ internal/transfer/instagram_transfer.go
@@ -15,3 +15,16 @@ type InstagramUserInfo struct {
 	Name           string `json:"name"`
 	ProfilePicture string `json:"profile_picture_url"`
 }
+
+type InstagramErrorResponse struct {
+	Error struct {
+		Message        string `json:"message"`
+		Type           string `json:"type"`
+		Code           int    `json:"code"`
+		ErrorSubcode   int    `json:"error_subcode"`
+		IsTransient    bool   `json:"is_transient"`
+		ErrorUserTitle string `json:"error_user_title"`
+		ErrorUserMsg   string `json:"error_user_msg"`
+		FbtraceID      string `json:"fbtrace_id"`
+	} `json:"error"`
+}
